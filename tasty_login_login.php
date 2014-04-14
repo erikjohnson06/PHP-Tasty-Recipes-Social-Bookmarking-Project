1ea28dd9f74975f622b16250f8091ff1691dafd5
ErikJohnson <erikjohnson06@gmail.com>
2014-04-14 07:53:36 -0400

    saving function files

---
+++ tasty_login_login.php
@@ -0,0 +1,10 @@
+<?php
+
+    $host = "localhost";
+    $socket = "/users/ejohnson4/mysql-php/data/mysql.sock";      
+    $port = 0;
+    $user = "tasty_login";
+    $pw = "scott123";
+    $database = "tasty_recipes";
+    
+?>
---
+++ tasty_member_login.php
@@ -0,0 +1,10 @@
+<?php
+
+    $host = "localhost";
+    $socket = "/users/ejohnson4/mysql-php/data/mysql.sock";      
+    $port = 0;
+    $user = "tasty_member";
+    $pw = "trish123";
+    $database = "tasty_recipes";
+    
+?>
